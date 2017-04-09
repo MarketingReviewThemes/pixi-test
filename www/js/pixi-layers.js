0e3d172cd96dc9015e35cfd6e748eb4dd48c7083
alexpro <tro_alex@mail.ru>
2017-04-09 21:42:55 +0300

    Adding moving event for figures

---
+++ www/js/pixi-layers.js
@@ -0,0 +1,3 @@
+/**
+ * Created by alexpro on 09.04.17.
+ */
--- www/js/view_v1.js
+++ www/js/view_v1.js
@@ -1,43 +1,37 @@
 /*global qs, qsa, $on, $parent, $delegate */
 
 (function (window) {
-    'use strict';
-
-    var WIDTH = 800,
-        HEIGHT = 600,
-        PAD = 100;
+        'use strict';
 
 
+    
     function View() {
-        this.createAppContainer();
+        this.getAppView();
 
         setInterval(() => {
             this.addFigure();
         }, 1000);
 
 
-        this.pixiApp.ticker.add((delta) => {
-            this.container.children.forEach(this.updatePosition);
-        });
+        // this.pixiApp.ticker.add((delta) => {
+        //     this.addFigure();
+        // });
+
+
     }
 
 
-    View.prototype.pixiApp = new PIXI.Application(WIDTH, HEIGHT);
-    
-    View.prototype.renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, {
+    View.prototype.renderer = PIXI.autoDetectRenderer(800, 600, {
         view: qs('#scene-box')
     });
-    View.prototype.container = new PIXI.Container();
+    View.prototype.container = new PIXI.Container(800, 600);
 
-
-    View.prototype.createAppContainer = function() {
+    View.prototype.getAppView = function() {
 
         this.container.interactive = true;
         this.container.buttonMode = true;
         this.container.id = 'container';
-
         this.container.on('pointerdown', (e) => {
-        //  this.pixiApp.stage.on('pointertap', (e) => {
 
             if(e.target.id == "circle") {
                 console.log("Circle clicked");
@@ -49,10 +43,10 @@
         });
 
 
-        //Rectangle for make container with rectangle size
+        //Rectangle for make container bigger
         var rectangle = new PIXI.Graphics();
         rectangle.beginFill(0xFFFFFF);
-        rectangle.drawRect(0, 0, WIDTH, HEIGHT);
+        rectangle.drawRect(0, 0, 800, 800);
 
         this.container.addChild(rectangle);
 
@@ -61,6 +55,14 @@
             that.renderer.render(that.container);
             requestAnimationFrame(animate);
         })();
+
+        // Click on scene
+
+        // this.pixiApp.view.interactive = true;
+        // this.pixiApp.view.addEventListener("click", (e) => {
+        //     console.log(e);
+        //     this.addFigure(e.clientX, e.clientY);
+        // });
     };
 
 
@@ -69,12 +71,6 @@
             radius = Math.floor(Math.random() * (this.renderer.height/8 - 1)) + 1,
             posX = posX || Math.floor(Math.random() * (this.renderer.width - 1)) + 1,
             posY = posY || Math.floor(Math.random() * (this.renderer.height - 1)) + 1;
-
-        var speed = 200.0; //px per second
-        circle.vx = 0;
-        circle.vy = speed / 60.0;
-
-        // circle.update = this.updatePosition;
 
         circle.beginFill(0xFF3300);
         circle.lineStyle(4, 0xffd900, 1);
@@ -85,20 +81,9 @@
         circle.buttonMode = true;
         circle.id = "circle";
         circle.on('click', (e) => {
-            e.target.destroy();
+            circle.destroy();
         });
         this.container.addChild(circle);
-    };
-
-
-    View.prototype.updatePosition = function(figure) {
-        // figure.x += figure.vx;
-        figure.y += figure.vy;
-
-        if (figure.y > HEIGHT + PAD) {
-            figure.y -= HEIGHT + 2 * PAD;
-        }
-
     };
 
 
